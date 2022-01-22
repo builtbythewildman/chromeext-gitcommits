994fb6ceaf9170210da274ff7b06183c85bb8296
Xander <xwernecke@gmail.com>
2022-01-22 11:12:28 +0100

    update
    updated manifest file and changed service worker file reference

--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
-let onLoad = (()=>{
-setTimeout(()=>{
-
+window.onload = ()=>{
 
 
 //pulling items from html
@@ -10,10 +8,6 @@ let cheatSheetBtn = document.querySelector(".cheatSheetBtn");
 let referencesBtn = document.querySelector(".referencesBtn");
 let listContent = document.querySelector(".listContent");
 
-//click the first button for the content to load on click
-document.addEventListener("DOMContentLoaded", function(){
-  commitMsgBtn.click()
-})
 //vars
 
 let cheatSheetArr = ["git init - initializes a brand new Git repository and begins tracking an existing directory. It adds a hidden subfolder within the existing directory that houses the internal data structure required for version control.", "git clone - creates a local copy of a project that already exists remotely. The clone includes all the project's files, history, and branches.", "git add - stages a change. Git tracks changes to a developer's codebase, but it's necessary to stage and take a snapshot of the changes to include them in the project's history. This command performs staging, the first part of that two-step process. Any changes that are staged will become a part of the next snapshot and a part of the project's history. Staging and committing separately gives developers complete control over the history of their project without changing how they code and work.", "git commit - saves the snapshot to the project history and completes the change-tracking process. In short, a commit functions like taking a photo. Anything that's been staged with git add will become a part of the snapshot with git commit.", "git status - shows the status of changes as untracked, modified, or staged.", "git branch - shows the branches being worked on locally.", "git merge - merges lines of development together. This command is typically used to combine changes made on two distinct branches. For example, a developer would merge when they want to combine changes from a feature branch into the main branch for deployment.", "git pull - updates the local line of development with updates from its remote counterpart. Developers use this command if a teammate has made commits to a branch on a remote, and they would like to reflect those changes in their local environment.", "git push - updates the remote repository with any commits made locally to a branch"];
@@ -56,6 +50,4 @@ referencesBtn.addEventListener("click", () => {
     listContent.appendChild(li)
   })
 });
-},2000);
-});
-onLoad();
+}
